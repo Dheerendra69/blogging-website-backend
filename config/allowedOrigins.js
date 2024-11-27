c4afb160f7085edea4c8fb9bccaf693a2b42c00c
Dheerendra <118679439+Dheerendra69@users.noreply.github.com>
2024-11-27 14:02:43 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-    'https://blogging-website-react.netlify.app'
+    'https://blogging-website-react.netlify.app',
+    'http://localhost:3000'
 
 ];
 
